8908bf9e0bdd6d566605234084d5855236b4cdca
KatePril <ket14pril14@gmail.com>
2023-06-17 19:10:41 +0300

    Add pattern replacement method, element delete method

--- src/app/models/AppModel.java
+++ src/app/models/AppModel.java
@@ -1,10 +1,25 @@
 package app.models;
 
+import java.util.ArrayList;
+
 public class AppModel {
 
-    public void executeTaskOne() {}
+    public String[] replacePatterns(String[] list, String pattern, String patternReplacement) {
+        for (int i = 0; i < list.length; i++) {
+            list[i] = list[i].replace(pattern, patternReplacement);
+        }
+        return list;
+    }
 
-    public void executeTaskTwo() {}
+    public String[] deleteElements(String[] list, String elementToBeDeleted) {
+        ArrayList<String> newList = new ArrayList<>();
+        for (String element : list) {
+            if (!element.equals(elementToBeDeleted)) {
+                newList.add(element);
+            }
+        }
+        return (String[]) newList.toArray();
+    }
 
     public void executeTaskThree() {}
 
